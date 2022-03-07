7e79af58d80e2424b36e505bb90165d766118f63
Hojoon Lee <leehj.david@gmail.com>
2022-03-07 23:56:47 +0900

    Initial commit

---
+++ src/main/java/me/cienowl/genistgarten/GenistGartenApplication.java
@@ -0,0 +1,13 @@
+package me.cienowl.genistgarten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenistGartenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenistGartenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/cienowl/genistgarten/GenistGartenApplicationTests.java
@@ -0,0 +1,13 @@
+package me.cienowl.genistgarten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenistGartenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
